0a1bc8529598f84f4f5e8ecf41a16cfc9c77e02a
nadimmorol890 <nadimmorol890@gmail.com>
2024-01-08 16:02:41 +0600

    add module 21

---
+++ Module_21/search_in_bst.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input_tree()
+{
+    int val;
+    cin >> val;
+    
+    Node * root;
+    if(val == -1) root = NULL;
+    else root = new Node(val);
+
+    queue<Node*> q;
+    if(root) q.push(root);
+
+    while(!q.empty())
+    {
+        Node * p = q.front();
+        q.pop();
+        
+        int l,r;
+        cin >> l >> r;
+        Node * myleft;
+        Node * myright;
+        
+        if(l == -1) 
+            myleft = NULL;
+        else 
+            myleft = new Node(l);
+        
+        if(r == -1)
+            myright = NULL;
+        else
+            myright = new Node(r);
+
+        p->left = myleft;
+        p->right = myright;
+        
+        if(p->left)
+            q.push(p->left);
+        if(p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+bool search(Node * root, int x)
+{
+    if(!root) return false;
+    
+    if(root->val == x) return true;
+
+    if(x < root->val)
+    {
+        return search(root->left, x);
+    }
+    else
+    {
+        return search(root->right, x);
+    }
+}
+int main()
+{
+    
+    int x;
+    cin >> x;
+    
+    Node * root = input_tree();
+
+    if(search(root, x))
+    {
+        cout << "Found" << endl;
+    }
+    else
+    {
+        cout << "Not Found" << endl;
+    }
+    return 0;
+}
